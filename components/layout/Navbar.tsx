93439acb8c0c37947841da7c204f3a69fb23b506
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2024-03-26 12:53:43 -0300

    display dashboard icon only for admin users
    Signed-off-by: René Ricardo <32307287+renericardoperez@users.noreply.github.com>

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,10 +1,13 @@
-import { CalendarDays, UserIcon } from "lucide-react"
+import { CalendarDays, LogInIcon, LayoutDashboard } from "lucide-react"
 import Link from "next/link"
 import { currentUser, SignInButton, UserButton } from "@clerk/nextjs"
 import { Button } from "@/components/ui/button"
+import { UserRole } from "@/lib/models"
 
 export async function Navbar() {
   const user = await currentUser()
+
+  const isAdmin = ((user?.privateMetadata?.role as string)?.toUpperCase() as UserRole) === "ADMIN"
 
   return (
     <div className={"bg-gray-50 w-screen h-auto py-2 md:py-3 px-5 md:px-8 border-b-2 border-b-gray-50"}>
@@ -19,14 +22,19 @@ export async function Navbar() {
           <Link href={"/calendar"} className={"hover:text-primary"} title="Ir al calendario">
             <CalendarDays />
           </Link>
+          {isAdmin ? (
+            <Link href={"/admin"} className={"hover:text-primary"} title="Ir al tablero">
+              <LayoutDashboard />
+            </Link>
+          ) : null}
           {!user ? (
             <SignInButton>
-              <Button variant={"ghost"} className={"hover:text-primary"} title="Inciar sesión">
-                <UserIcon />
+              <Button variant={"ghost"} className={"hover:text-primary -mr-[16px]"} title="Inciar sesión">
+                <LogInIcon />
               </Button>
             </SignInButton>
           ) : null}
-          <UserButton />
+          {user ? <UserButton /> : null}
         </div>
       </div>
     </div>
