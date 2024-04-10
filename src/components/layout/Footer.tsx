5014019bfa946df9bbc3e4c6f90967d29258d789
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2024-04-09 22:28:59 -0300

    update website link at footer
    Signed-off-by: René Ricardo <32307287+renericardoperez@users.noreply.github.com>

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,11 @@ export const Footer = () => {
     <div className={"bg-gray-50 w-100 h-auto mt-auto py-4 px-5 text-black border-t-2 border-t-gray-50"}>
       <div className={"flex justify-center items-center text-center"}>
         <div className={"text-sm"}>
-          <a href="https://renerp.dev" target="_blank" rel="noopener" className={"font-semibold hover:text-primary"}>
+          <a
+            href="https://portfolio.renerp.dev"
+            target="_blank"
+            rel="noopener"
+            className={"font-semibold hover:text-primary"}>
             renerp.dev
           </a>{" "}
           <span>{t("copyright", { year: new Date().getFullYear() })}</span>
